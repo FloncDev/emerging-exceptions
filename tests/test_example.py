fb0afde2da76769ad8c41f16f6aafe3446440bde
i-am-unknown-81514525 <74453352+i-am-unknown-81514525@users.noreply.github.com>
2023-08-31 16:47:04 +0100

    Fix forgotten D400 flake8 rule

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,8 +1,8 @@
-"""Test pre-commit GitHub Action and flake8 Code Style Checker"""
+"""Test pre-commit GitHub Action and flake8 Code Style Checker."""
 # Example import
 from math import pi, sin
 
 
 def test_sin():
-    """A simple example test to test GitHub actions"""
+    """A simple example test to test GitHub actions."""
     assert sin(pi / 2) == 1.0
