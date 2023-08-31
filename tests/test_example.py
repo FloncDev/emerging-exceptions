297111279c8562cc75d56159009364c5a93ed2c8
i-am-unknown-81514525 <74453352+i-am-unknown-81514525@users.noreply.github.com>
2023-08-31 16:33:23 +0100

    Match all flake8 rules

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,7 +1,8 @@
+"""Test pre-commit GitHub Action and flake8 Code Style Checker"""
 # Example import
 from math import pi, sin
 
 
 def test_sin():
     """A simple example test to test GitHub actions"""
-    assert sin(pi/2) == 1.0
+    assert sin(pi / 2) == 1.0
