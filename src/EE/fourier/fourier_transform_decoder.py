96d193edabbad39e19dfcc1842da69224cd80008
Flonc <flonc@DESKTOP-2923VTQ.localdomain>
2023-09-10 00:13:50 +0100

    🚧 Some formatting (still needs docstrings)

--- src/EE/fourier/fourier_transform_decoder.py
+++ src/EE/fourier/fourier_transform_decoder.py
@@ -1,5 +1,6 @@
-from PIL import Image
 import numpy as np
+from PIL import Image
+
 
 def load_image():
     image_path = input("Enter the path to the watermarked image file: ")
@@ -9,21 +10,22 @@ def load_image():
         print("Image not found. Exiting.")
         exit()
 
+
 def fourier_transform_color(image):
     image_array = np.array(image)
-    red, green, blue = image_array[:,:,0], image_array[:,:,1], image_array[:,:,2]
+    red, green, blue = image_array[:, :, 0], image_array[:, :, 1], image_array[:, :, 2]
     red_freq = np.fft.fft2(red)
     green_freq = np.fft.fft2(green)
     blue_freq = np.fft.fft2(blue)
     return red_freq, green_freq, blue_freq
 
+
 def extract_watermark(frequency_domain, watermark_length):
     magnitude = np.abs(frequency_domain)
     watermark_data = magnitude[0, 0:watermark_length]
     print(f"Debug: Extracted data from magnitude: {watermark_data}")
-    watermark_text = ''.join([chr(int(round(x))) for x in watermark_data])
+    watermark_text = "".join([chr(int(round(x))) for x in watermark_data])
     return watermark_text
-
 
 
 def main():
@@ -32,12 +34,17 @@ def main():
     watermark_length = len(watermark_text)
 
     try:
-        loaded_red_freq_watermarked = np.load(f"red_freq_watermarked_{watermark_text}.npy")
-        watermark_text_red = extract_watermark(loaded_red_freq_watermarked, watermark_length)
+        loaded_red_freq_watermarked = np.load(
+            f"red_freq_watermarked_{watermark_text}.npy"
+        )
+        watermark_text_red = extract_watermark(
+            loaded_red_freq_watermarked, watermark_length
+        )
         print(f"Extracted watermark text from the red channel is: {watermark_text_red}")
     except FileNotFoundError:
         print("Frequency domain data not found. Exiting.")
         exit()
+
 
 if __name__ == "__main__":
     main()
--- src/EE/fourier/main.py
+++ src/EE/fourier/main.py
@@ -1,5 +1,5 @@
-from PIL import Image
 import numpy as np
+from PIL import Image
 
 
 def get_watermark_text():
@@ -13,12 +13,14 @@ def load_image():
     except FileNotFoundError:
         print("Image not found. Please check the path and try again.")
         return None
+
+
 def shift_to_center(frequency_domain):
     return np.fft.fftshift(frequency_domain)
 
+
 def shift_to_corners(frequency_domain):
     return np.fft.ifftshift(frequency_domain)
-
 
 
 def fourier_transform_color(image):
@@ -30,15 +32,16 @@ def fourier_transform_color(image):
     return red_freq, green_freq, blue_freq
 
 
-
 def embed_watermark_to_color_channel(magnitude, phase, watermark_array):
     # Embed watermark into magnitude
-    magnitude[0:1, 0:watermark_array.shape[1]] = watermark_array
+    magnitude[0:1, 0 : watermark_array.shape[1]] = watermark_array
 
     # Print debug information
     # print("Debug: Portion of magnitude where watermark is embedded:")
     # print(magnitude[0:1, 0:watermark_array.shape[1]])
-    print(f"Debug: Portion of magnitude where watermark is embedded:\n{magnitude[0:1, 0:watermark_array.shape[1]]}")
+    print(
+        f"Debug: Portion of magnitude where watermark is embedded:\n{magnitude[0:1, 0:watermark_array.shape[1]]}"
+    )
     frequency_domain_watermarked = magnitude * np.exp(1j * phase)
 
     return frequency_domain_watermarked
@@ -50,7 +53,7 @@ def inverse_fourier_transform_color(red_freq, green_freq, blue_freq):
     blue = np.fft.ifft2(shift_to_corners(blue_freq))
     color_image_array = np.stack([np.real(red), np.real(green), np.real(blue)], axis=2)
     color_image_array = np.uint8(color_image_array)
-    return Image.fromarray(color_image_array, 'RGB')
+    return Image.fromarray(color_image_array, "RGB")
 
 
 def save_image(image, watermark_text):
@@ -60,6 +63,7 @@ def save_image(image, watermark_text):
 
     except Exception as e:
         print(f"Failed to save image: {e}")
+
 
 def main():
     watermark_text = get_watermark_text()
@@ -71,7 +75,6 @@ def main():
         red_freq, green_freq, blue_freq = fourier_transform_color(image)
         print("Successfully loaded image to frequency domain.")
 
-
         watermark_array = np.array([[ord(char) for char in watermark_text]])
 
         # Separate magnitude and phase for each color channel
@@ -80,14 +83,22 @@ def main():
         blue_magnitude, blue_phase = np.abs(blue_freq), np.angle(blue_freq)
 
         # Embed the watermark and get the watermarked frequency domain
-        red_freq_watermarked = embed_watermark_to_color_channel(red_magnitude, red_phase, watermark_array)
-        green_freq_watermarked = embed_watermark_to_color_channel(green_magnitude, green_phase, watermark_array)
-        blue_freq_watermarked = embed_watermark_to_color_channel(blue_magnitude, blue_phase, watermark_array)
+        red_freq_watermarked = embed_watermark_to_color_channel(
+            red_magnitude, red_phase, watermark_array
+        )
+        green_freq_watermarked = embed_watermark_to_color_channel(
+            green_magnitude, green_phase, watermark_array
+        )
+        blue_freq_watermarked = embed_watermark_to_color_channel(
+            blue_magnitude, blue_phase, watermark_array
+        )
         # Save the frequency domain data to disk
         np.save(f"red_freq_watermarked_{watermark_text}.npy", red_freq_watermarked)
 
         print("Successfully embedded watermark into frequency domain.")
-        print(f"Debug: Red frequency domain after watermark: {np.abs(red_freq_watermarked)[0, 0:10]}")
+        print(
+            f"Debug: Red frequency domain after watermark: {np.abs(red_freq_watermarked)[0, 0:10]}"
+        )
 
         watermarked_image = inverse_fourier_transform_color(
             red_freq_watermarked, green_freq_watermarked, blue_freq_watermarked
@@ -101,6 +112,7 @@ def main():
         print(f"Debug: Loaded red frequency domain: {np.abs(loaded_red_freq)[0, 0:10]}")
     else:
         print("Failed to load image. Exiting.")
+
 
 if __name__ == "__main__":
     main()
